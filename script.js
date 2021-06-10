a0eb5cf0ea28c0dd16613cec98ffa7f8ab17f046
YJulyXing <41949986+YJulyXing@users.noreply.github.com>
2021-06-09 21:26:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear()+parseInt(years); 
-    if (principal <= 0) {
-        alert("Please enter a positive principal");
+    var interest = principal * years * (rate / 100);
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+
+    if (principal <=0){
+        alert("Enter a positive number");
         document.getElementById("principal").focus();
-        return false;
-     }
-    document.getElementById("result").innerHTML=
-        "\<br\> If you deposit \<span id=\"lb1\"\>" + principal + "\<\/span\>, \<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year "+year+"\<br\>";
+    }
+    else {
+        result.innerHTML = "If you deposit <b><mark>"+principal+"<\/mark><\/b>,<\/br>at an interest rate of <b><mark>"+rate+"%<\/mark><\/b>.<\/br>You will receive an amount of <b><mark>"+interest+"<\/mark><\/b>,<\/br>in the year <b><mark>"+year;+"<\/mark><\/b><\/br>";
+    }
 }
 
-function updaterate()
- {
-     var rateval = document.getElementById("rate").value;
-     document.getElementById("rate_val").innerText  = rateval + "%";
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
+}
 
- }
-
-        
